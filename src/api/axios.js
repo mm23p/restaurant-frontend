d7a96a5b1721df2b7f0ae8981bbf13f076c27ee4
mm23p <maryamawit81@gmail.com>
2025-07-07 02:17:51 +0300

    Update axios config and relocate .env

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,34 +1,3 @@
-/* import axios from 'axios';
-
-// Create a new instance of axios with a custom configuration
-const axiosInstance = axios.create({
-  // This sets the base URL for all API calls
-  baseURL: 'http://localhost:5000/api', 
-});
-
-// This is the "interceptor". It's a special function that runs
-// BEFORE every single request is sent.
-axiosInstance.interceptors.request.use(
-  (config) => {
-    // Get the token that was saved in localStorage during login
-    const token = localStorage.getItem('token');
-    
-    if (token) {
-      // If a token exists, add it to the 'Authorization' header
-      config.headers['Authorization'] = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => {
-    // Do something with request error
-    return Promise.reject(error);
-  }
-);
-
-export default axiosInstance; */
-
-// In src/api/axios.js
-
 import axios from 'axios';
 
 // Get the backend API URL from the environment variable.
